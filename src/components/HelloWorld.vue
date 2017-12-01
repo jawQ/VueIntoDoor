9be689c1b0eb769d91c886763f53be30738ae523
wjq <jawq@foxmail.com>
2017-12-02 02:04:29 +0800

    学习require....

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,155 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <cube-index-list
+    :data="cityData"
+    :title="title"
+    @select="selectItem"
+    @title-click="clickTitle">点击点击我~</cube-index-list>
+  </div>
+</template>
+
+<script>
+const cityData = [
+  {
+    "name": "★Hot City",
+    "items": [
+      {
+        "name": "BEIJING",
+        "value": 1
+      },
+      {
+        "name": "SHANGHAI",
+        "value": 2
+      }
+    ]
+  },
+  {
+    "name": "A",
+    "items": [
+      {
+        "name": "ANSHAN",
+        "value": 3
+      },
+      {
+        "name": "ANQING",
+        "value": 4
+      }
+    ]
+  },
+  {
+    "name": "A",
+    "items": [
+      {
+        "name": "ANSHAN",
+        "value": 3
+      },
+      {
+        "name": "ANQING",
+        "value": 4
+      }
+    ]
+  },
+  {
+    "name": "A",
+    "items": [
+      {
+        "name": "ANSHAN",
+        "value": 3
+      },
+      {
+        "name": "ANQING",
+        "value": 4
+      }
+    ]
+  },
+  {
+    "name": "A",
+    "items": [
+      {
+        "name": "ANSHAN",
+        "value": 3
+      },
+      {
+        "name": "ANQING",
+        "value": 4
+      }
+    ]
+  },
+  {
+    "name": "A",
+    "items": [
+      {
+        "name": "ANSHAN",
+        "value": 3
+      },
+      {
+        "name": "ANQING",
+        "value": 4
+      }
+    ]
+  },
+  {
+    "name": "A",
+    "items": [
+      {
+        "name": "ANSHAN",
+        "value": 3
+      },
+      {
+        "name": "ANQING",
+        "value": 4
+      }
+    ]
+  },{
+    "name": "c",
+    "items": [
+      {
+        "name": "ANSHAN",
+        "value": 3
+      },
+      {
+        "name": "ANQING",
+        "value": 4
+      }
+    ]
+  }
+
+]
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      title: 'Current City: BEIJING',
+      cityData: cityData
+    }
+  },
+  methods: {
+    selectItem(item) {
+      console.log(item.name)
+    },
+    clickTitle(title) {
+      console.log(title)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
